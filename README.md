39751bb8d22cae12e7783342ebe89f30673595e5
Haoming <hmstudy02@gmail.com>
2026-03-11 16:24:11 +0800

    update

--- README.md
+++ README.md
@@ -1,24 +1,46 @@
 # SD Forge Nvidia VFX
-This is an Extension for [Forge Neo](https://github.com/Haoming02/sd-webui-forge-classic/tree/neo), which implements **VideoSuperRes** from [nvidia-vfx](https://pypi.org/project/nvidia-vfx/) to achieve blazingly fast high quality **image upscale**
+This is an Extension for [Forge Neo](https://github.com/Haoming02/sd-webui-forge-classic/tree/neo), which implements **VideoSuperRes** from [nvidia-vfx](https://pypi.org/project/nvidia-vfx/) to achieve blazingly-fast high-quality **image enhancement**
+
+> Also supports [Forge Classic](https://github.com/Haoming02/sd-webui-forge-classic/tree/classic), [Automatic1111 Webui](https://github.com/AUTOMATIC1111/stable-diffusion-webui), etc.
 
 > [!Important]
-> Only supports Nvidia GPU
+> Nvidia GPU is Required
 
 <table>
   <caption>
-    <ins><b>Ultra</b> Quality</ins>
-    with
-    <ins><b>x4</b> Scale</ins>
-    took
-    <b>1.2s</b>
-    on RTX 3060
+    <b>Ultra</b> took <b>1.2s</b> on a <b>RTX 3060</b>
   </caption>
   <tr>
-    <th>512x512</th>
-    <th>2048x2048</th>
+    <th>256x256</th>
+    <th>1024x1024</th>
   </tr>
   <tr>
-    <td><img width="512" src="input.webp" /></td>
-    <td><img width="512" src="output.webp" /></td>
+    <td><img width="512" src="input.jpg" /></td>
+    <td><img width="512" src="output.jpg" /></td>
   </tr>
 </table>
+
+## Features
+> Select the preset from the `Upscaler` dropdown in **Extras**
+
+> [!Tip]
+> `Scale by` mode is recommended
+
+#### Upscale
+
+- Super-resolution an image by the desired amount
+- `Resize` must be divisible by `0.25`
+- The **HD** variants are meant for high quality images with no compression artifacts
+
+#### Deblur
+
+- Sharpen an image
+- `Resize` must be `1.0`
+
+#### Denoise
+
+- Remove noises from an image
+- `Resize` must be `1.0`
+
+## TODO
+- [ ] Support Video
--- scripts/rtx.py
+++ scripts/rtx.py
@@ -1,52 +1,62 @@
-import gradio as gr
+from functools import wraps
+
 import numpy as np
 import nvvfx
 import torch
 from PIL import Image
 
-from backend import memory_management
-from modules import scripts_postprocessing
-from modules.ui_components import InputAccordion
-
-assert memory_management.is_nvidia(), "Only Nvidia GPU is supported..."
+from modules import devices, modelloader, shared
+from modules.upscaler import Upscaler, UpscalerData
 
 
-class ForgeRTX(scripts_postprocessing.ScriptPostprocessing):
+class UpscalerNvidia(Upscaler):
     name = "Nvidia VFX"
-    order = 1024
 
-    def ui(self):
-        with InputAccordion(value=False, label="Nvidia VFX") as enable:
-            with gr.Row():
-                quality = gr.Dropdown(
-                    choices=("Low", "Medium", "High", "Ultra"),
-                    value="High",
-                    label="Quality",
-                )
-                scale = gr.Radio(
-                    choices=("x2", "x3", "x4"),
-                    value="x2",
-                    label="Scale by",
-                )
+    def __init__(self):
+        super().__init__(False)
 
-        return {"enable": enable, "quality": quality, "scale": scale}
+        self.scalers = [
+            # Standard Upscaling
+            UpscalerData("[Nvidia] Speed", "LOW", self, 2),
+            UpscalerData("[Nvidia] Balanced", "MEDIUM", self, 2),
+            UpscalerData("[Nvidia] Quality", "HIGH", self, 2),
+            UpscalerData("[Nvidia] Ultra", "ULTRA", self, 4),
+            # Denoise
+            UpscalerData("[Nvidia] Denoise Light", "DENOISE_LOW", self, 1),
+            UpscalerData("[Nvidia] Denoise Moderate", "DENOISE_MEDIUM", self, 1),
+            UpscalerData("[Nvidia] Denoise Aggressive", "DENOISE_HIGH", self, 1),
+            UpscalerData("[Nvidia] Denoise Maximum", "DENOISE_ULTRA", self, 1),
+            # Deblur
+            UpscalerData("[Nvidia] Deblur Light", "DEBLUR_LOW", self, 1),
+            UpscalerData("[Nvidia] Deblur Moderate", "DEBLUR_MEDIUM", self, 1),
+            UpscalerData("[Nvidia] Deblur Aggressive", "DEBLUR_HIGH", self, 1),
+            UpscalerData("[Nvidia] Deblur Maximum", "DEBLUR_ULTRA", self, 1),
+            # High-Bitrate
+            UpscalerData("[Nvidia] HD Speed", "HIGHBITRATE_LOW", self, 2),
+            UpscalerData("[Nvidia] HD Balanced", "HIGHBITRATE_MEDIUM", self, 2),
+            UpscalerData("[Nvidia] HD Quality", "HIGHBITRATE_HIGH", self, 2),
+            UpscalerData("[Nvidia] HD Ultra", "HIGHBITRATE_ULTRA", self, 4),
+        ]
 
-    def process(self, pp: scripts_postprocessing.PostprocessedImage, **args):
-        if not args.pop("enable", False):
-            return
+    def upscale(self, img: Image.Image, scale: float, selected_model: str):
+        image: Image.Image = img.convert("RGB")
+        quality = getattr(nvvfx.effects.QualityLevel, selected_model)
 
-        _quality: str = args.get("quality", "High")
-        _scale: str = args.get("scale", "x2")
-
-        image: Image.Image = pp.image.convert("RGB")
-        quality = getattr(nvvfx.effects.QualityLevel, _quality.upper())
-        scale = int(_scale[-1])
+        if selected_model.startswith(("DENOISE", "DEBLUR")):
+            if scale != 1.0:
+                scale = 1.0
+                print("[Warning] Denoise/Deblur requires Scale = 1.0")
+        else:
+            if scale % 0.25 != 0.0:
+                scale = round(scale / 0.25) * 0.25
+                print(f"[Warning] Scale must be divisible by 0.25 (changed to {scale})")
 
         orig_w, orig_h = image.size
-        target_w, target_h = orig_w * scale, orig_h * scale
+        target_w, target_h = int(orig_w * scale), int(orig_h * scale)
 
-        device = memory_management.get_torch_device()
-        memory_management.soft_empty_cache()
+        device = devices.device
+        assert device.type == "cuda"
+        devices.torch_gc()
 
         ndarray: np.ndarray = np.asarray(image, dtype=np.uint8)
         tensor: torch.Tensor = torch.from_numpy(ndarray)
@@ -65,7 +75,25 @@ class ForgeRTX(scripts_postprocessing.ScriptPostprocessing):
         tensor = output.clamp(0.0, 1.0).permute(1, 2, 0)
         image = (tensor.cpu().numpy() * 255.0).astype("uint8")
 
-        memory_management.soft_empty_cache()
+        return Image.fromarray(image)
 
-        pp.image = Image.fromarray(image)
-        pp.info["NvidiaVFX"] = args.copy()
+    def do_upscale(self, *args, **kwargs):
+        raise SyntaxError
+
+    def load_model(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def find_models(self, *args, **kwargs):
+        raise NotImplementedError
+
+
+orig = modelloader.load_upscalers
+
+
+@wraps(orig)
+def extra_upscalers():
+    orig()
+    shared.sd_upscalers.extend(UpscalerNvidia().scalers)
+
+
+modelloader.load_upscalers = extra_upscalers
